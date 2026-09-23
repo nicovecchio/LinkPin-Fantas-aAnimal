e0bc4ba1cd5a98656bccea13038037234fcb00b1
nicovecchio <nicoladelvecchio123@gmail.com>
2026-09-23 13:25:31 -0400

    Add TikTokIcon component to brand-icons

--- components/brand-icons.tsx
+++ components/brand-icons.tsx
@@ -56,3 +56,20 @@ export function GitHubIcon(props: IconProps) {
     </svg>
   )
 }
+import type { SVGProps } from "react"
+
+export function TikTokIcon(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="currentColor"
+      {...props}
+    >
+      <path d="M19.59 6.69a4.83 4.83 0 0 1-3.77-4.25V2h-3.45v13.67a2.89 2.89 0 1 1-5.2-1.74 2.89 2.89 0 0 1 2.31-2.22V8.2a6.34 6.34 0 1 0 6.34 6.34V9.33a8.28 8.28 0 0 0 4.77 1.52V7.4a4.85 4.85 0 0 1-1-.71z" />
+    </svg>
+  )
+}
+
